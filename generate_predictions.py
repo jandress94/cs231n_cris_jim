88faf1415f1ad80a84832c4f628265e170b6b1cf
Cristian Zanoci <czanoci@gmail.com>
2017-06-05 05:08:28 -0700

    now generate_prediction also uses the whole data set to detrmine thresholds

--- generate_predictions.py
+++ generate_predictions.py
@@ -11,10 +11,13 @@ import pandas as pd
 import torchvision
 import torchvision.transforms as T
 from MultiLabelImageFolderTest import *
+from MultiLabelImageFolder import *
 from torchvision.datasets import ImageFolder
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('--train_dir', default='../cs231n_data/train-jpg-all/')
+parser.add_argument('--train_labels_file', default='../cs231n_data/train_v2-all.csv')
 parser.add_argument('--test_dir', default='../cs231n_data/test-jpg/')
 parser.add_argument('--sub_file', default='../cs231n_data/submission.csv')
 parser.add_argument('--label_list_file', default = '../cs231n_data/labels.txt')
@@ -36,6 +39,51 @@ def find_classes(label_list_file):
     classes = np.array([line.strip() for line in f])
     f.close()
     return classes
+
+def compute_f2(scores, y, threshold, axis, eps = 1e-8):
+  preds = scores >= threshold
+  preds = preds.cpu().int()
+  y = y.int()
+    
+  pred_pos = torch.sum(preds, axis).float()
+  real_pos = torch.sum(y, axis).float()
+  true_pos = torch.sum(y * preds, axis).float()
+  
+  p = 1.0 * true_pos / (pred_pos + eps)
+  r = 1.0 * true_pos / (real_pos + eps)
+
+  beta = 2
+  return (1.0 + beta**2)*p*r / (beta**2 * p + r + eps)
+
+def recompute_thresholds(model, loader, dtype, eps = 1e-8):
+  scores_list = []
+  ys = []
+
+  for x, y in loader:
+    x_var = Variable(x.type(dtype), volatile = True)
+    scores = model(x_var)
+    normalized_scores = torch.sigmoid(scores)
+    scores_list.append(normalized_scores)
+    ys.append(y)
+
+  scores = torch.cat(scores_list, 0).data
+  ys = torch.cat(ys, 0)
+
+  best_thresh = np.zeros((17,))
+  best_f2 = -np.ones((17,))
+
+  for t in range(1000):
+    print_progress(t, 1000, 'Recomputing thresholds')
+    thresh = (1 + t) * 0.001
+
+    f2 = compute_f2(scores, ys, thresh, 0).numpy()
+
+    better_mask = f2 > best_f2
+    better_mask = better_mask.astype(np.int)
+    best_thresh = (1 - better_mask) * best_thresh + better_mask * thresh
+    best_f2 = (1 - better_mask) * best_f2 + better_mask * f2
+
+  return best_thresh
 
 def main(args):
   global label_thresholds
@@ -61,6 +109,57 @@ def main(args):
                   batch_size=args.batch_size,
                   num_workers=args.num_workers)
 
+
+  # Use the torchvision.transforms package to set up a transformation to use
+  # for our images at training time. The train-time transform will incorporate
+  # data augmentation and preprocessing. At training time we will perform the
+  # following preprocessing on our images:
+  # (1) Resize the image so its smaller side is 256 pixels long
+  # (2) Take a random 224 x 224 crop to the scaled image
+  # (3) Horizontally flip the image with probability 1/2
+  # (4) Convert the image from a PIL Image to a Torch Tensor
+  # (5) Normalize the image using the mean and variance of each color channel
+  #     computed on the ImageNet dataset.
+  train_transform = T.Compose([
+    T.Scale(256),
+    T.RandomSizedCrop(224),
+    T.RandomHorizontalFlip(),
+    T.ToTensor(),            
+    T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+  ])
+
+  def transform_target_to_1_0_vect(target):
+    vect = np.zeros((17,))
+    vect[target] = 1
+    return vect
+  
+  # You load data in PyTorch by first constructing a Dataset object which
+  # knows how to load individual data points (images and labels) and apply a
+  # transform. The Dataset object is then wrapped in a DataLoader, which iterates
+  # over the Dataset to construct minibatches. The num_workers flag to the
+  # DataLoader constructor is the number of background threads to use for loading
+  # data; this allows dataloading to happen off the main thread. You can see the
+  # definition for the base Dataset class here:
+  # https://github.com/pytorch/pytorch/blob/master/torch/utils/data/dataset.py
+  #
+  # and you can see the definition for the DataLoader class here:
+  # https://github.com/pytorch/pytorch/blob/master/torch/utils/data/dataloader.py#L262
+  #
+  # The torchvision package provides an ImageFolder Dataset class which knows
+  # how to read images off disk, where the image from each category are stored
+  # in a subdirectory.
+  #
+  # You can read more about the ImageFolder class here:
+  # https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py
+  train_dset = MultiLabelImageFolder(args.train_dir, args.train_labels_file, args.label_list_file, \
+    transform=train_transform, target_transform = transform_target_to_1_0_vect)
+
+  
+  train_loader = DataLoader(train_dset,
+                    batch_size=args.batch_size,
+                    num_workers=args.num_workers,
+                    shuffle=True)
+
   # Now that we have set up the data, it's time to set up the model.
   # For this example we will finetune a densenet-169 model which has been
   # pretrained on ImageNet. We will first reinitialize the last layer of the
@@ -69,7 +168,7 @@ def main(args):
 
   # First load the pretrained densenet-169 model; this will download the model
   # weights from the web the first time you run it.
-  model = torchvision.models.densenet169(pretrained=True)
+  model = torchvision.models.resnet18(pretrained=True)
 
   # Reinitialize the last layer of the model. Each pretrained model has a
   # slightly different structure, but from the densenet class definition
@@ -85,8 +184,9 @@ def main(args):
 
   model.eval()
 
+  label_thresholds = recompute_thresholds(model, train_loader, dtype)
 
-  label_thresholds = np.load(args.save_thresholds_path, allow_pickle = False)
+  #label_thresholds = np.load(args.save_thresholds_path, allow_pickle = False)
   thresholds = torch.Tensor(label_thresholds).type(dtype)
   classes = find_classes(args.label_list_file)
 
